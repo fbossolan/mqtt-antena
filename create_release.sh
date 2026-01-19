5ea22e114e3fc2396ffe9814af9b22f5c35fdb7b
buzz <buzz@buzz.com>
2026-01-18 21:59:46 -0300

    feat: fix broken steps in the release script

--- create_release.sh
+++ create_release.sh
@@ -26,23 +26,28 @@ fi
 git fetch origin && \
 [ -z "$(git status --porcelain)" ] || { echo "There are uncommitted changes"; exit 1; } && \
 git merge-base --is-ancestor origin/main HEAD && \
-echo "OK to merge" || \
-echo "Rebase or merge main first" && exit 1
+echo "OK to merge" || echo "Rebase or merge main first" && exit 1
 
 
 # Updates main & merges
+echo "Updating main"
 git pull origin main
+
+echo "Merging $BRANCH into main"
 git merge $BRANCH
 
 # generating changelog
+echo "Generating changelog"
 git cliff -o CHANGELOG.md
 git add CHANGELOG.md
 git commit -m "chore: update changelog"
 
 # Creates annotated tag
+echo "Creating tag v$VERSION"
 git tag -a "v$VERSION" -m "Release v$VERSION"
 
 # Push branch + tag
+echo "Pushing branch + tag"
 git push origin main
 git push origin "v$VERSION"
 
